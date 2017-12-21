0fdfe5e794f070e96e058f8fbaeec3b587ff92ad
Niraj Madanmohan Kashyap <nirajka@cybage.com>
2017-12-21 21:51:18 +0530

    seqence change

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import * as TodoActions from '../../actions/todos';
 import * as style from './style.css';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
+
+import * as TodoActions from '../../actions/todos';
 import { RootState } from '../../reducers';
 import { Header, MainSection } from '../../components';
 
